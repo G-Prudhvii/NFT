380770ff85d548fc64295ddf39f678ca5b0ba2b4
prudhvi <prudhvi@M1-Air.local>
2024-04-23 23:26:24 +0530

    Added Dynamic Mood Nft Contract

---
+++ script/Interactions.s.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+// import "../lib/forge-std/Script.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+
+contract MintBasicNft is Script {
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNft",
+            block.chainid
+        );
+        mintNftOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(PUG_URI);
+        vm.stopBroadcast();
+    }
+}
+
+contract MintMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "MoodNFT",
+            block.chainid
+        );
+        mintNftOnContract(mostRecentlyDeployed);
+    }
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        MoodNft(contractAddress).mintNft();
+        vm.stopBroadcast();
+    }
+}
--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -2,20 +2,27 @@
 pragma solidity ^0.8.24;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
+// import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 contract BasicNft is ERC721 {
     uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenIdToUri;
     constructor() ERC721("Dogie", "DOG") {
         s_tokenCounter = 0;
     }
 
-    function mintNft() public {}
+    function mintNft(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
 
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
-        return "";
+        return s_tokenIdToUri[tokenId];
     }
 
-    function getTokenCounter(address _owner)
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
 }
